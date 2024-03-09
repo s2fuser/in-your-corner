08c1d979f720ecea4622fbfcb420afb7ba395488
Usman <usman@vecan.com>
2024-03-09 12:04:46 +0530

    initial design

---
+++ in-your-corner/src/Components/BrowseVideoPage/BrowseVideo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface BrowseVideoProps {
+  // Add any props you need for your component
+}
+
+const BrowseVideoPage: React.FC<BrowseVideoProps> = (props) => {
+  // Your component logic goes here
+
+  return (
+    <div>
+      <h1>BROWSE BY TOPIC</h1>
+      {/* Add your JSX for displaying videos or any other content */}
+    </div>
+  );
+};
+
+export default BrowseVideoPage;
