ce028e4d4743366d216d7b16eaa433925e8b03c7
s2fuser <git@s2ftech.co.in>
2024-03-09 12:04:06 +0530

    adding design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Components/Header/Header';
+import Mainheading from './Components/Mainheading';
+import NavigationBar from './Components/NavigationBar/NavigationBar';
+import Footer from './Components/Footer/Footer';
+import Footer2 from './Components/Footer2/Footer2';
 
 function App() {
   return (
-    <div className="">      
+    <div className=""> 
+    <Header/>
+    <NavigationBar/>
+    <Mainheading/>
+    <Footer/>
+    <Footer2/>
+
     </div>
   );
 }
