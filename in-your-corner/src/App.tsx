a908d0c29215ddbf79f82fdf7ae4a7d9860bbcd9
Usman <usman@vecan.com>
2024-03-09 13:32:23 +0530

    added topic buttons

--- in-your-corner/src/App.tsx
+++ in-your-corner/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import BrowseVideoPage from './Components/BrowseVideoPage/BrowseVideo';
 import './App.css';
 
 function App() {
   return (
-    <div className="">      
+    <div className="">     
+    <BrowseVideoPage/>  
     </div>
   );
 }
