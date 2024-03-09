b3db5fa237fcdadd256be3f2af7248f7f3072e87
s2fuser <git@s2ftech.co.in>
2024-03-09 13:31:51 +0530

    design

--- src/Components/Mainheading.tsx
+++ src/Components/Mainheading.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Mainheading = () => {
     return (
-        <div>
-            <h1 className=' text-4xl font-semibold text-center'>
+        <div className='bg-red-100 w- full h-80 font-serif'>
+            <h1 className=' text-4xl font-semibold text-center mt-1 pt-11'>
                WELCOME TO OUR VIDEO LIBARY
             </h1>
             
-            <h3 className='text-red-600 text-2xl text-center'>
+            <h3 className='text-red-600 text-2xl text-center pt-6'>
                 A free streaming library of over 150 films <br/> featuring the real stories of real people who met the real Jesus.
             </h3>
-            <h1 className='text-4xl font-semibold text-center' >
+            <h1 className='text-4xl font-semibold text-center pt-16' >
                 FEATURED FILMS
             </h1>
         </div>
