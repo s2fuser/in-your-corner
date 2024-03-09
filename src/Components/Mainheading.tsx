140cdcce21852e1fa1573ffcd1bcdf69c6fafeb5
s2fuser <git@s2ftech.co.in>
2024-03-09 11:08:03 +0530

    header and footer design

---
+++ src/Components/Mainheading.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Mainheading = () => {
+    return (
+        <div>
+            <h1 className=' text-4xl font-semibold text-center'>
+               WELCOME TO OUR VIDEO LIBARY
+            </h1>
+            
+            <h3 className='text-red-600 text-2xl text-center'>
+                A free streaming library of over 150 films <br/> featuring the real stories of real people who met the real Jesus.
+            </h3>
+            <h1 className='text-4xl font-semibold text-center' >
+                FEATURED FILMS
+            </h1>
+        </div>
+    )
+}
+
+export default Mainheading
