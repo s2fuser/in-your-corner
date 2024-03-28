5ecbb4f40aafafa5b46cc72700d11703065f48eb
s2fuser <git@s2ftech.co.in>
2024-03-28 17:22:41 +0530

    animation changes

--- src/Components/Moviesimage/Movierow2.tsx
+++ src/Components/Moviesimage/Movierow2.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import VideoSlider from "../VideoSlider/VideoSlider";
 import VideoSlider2 from "../VideoSlider2/VideoSlider2";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Movierow2 = () => {
   return (
     <div className="">
-      <div className="w-full h-120 overflow-x-hidden">
-        <h1 className="raleway text-2xl font-semibold text-center mt-1 pt-24 font-sans sm:text-3xl md:text-3xl lg:text-3xl xl:text-3xl 2xl:text-4xl sm:mt-[-3%]">
+      <div className="w-full h-120 overflow-x-hidden ">
+        <h1
+          className="raleway text-2xl font-semibold text-center mt-1 pt-24 font-sans sm:text-3xl md:text-3xl lg:text-3xl mt-[-45px] xl:text-3xl 2xl:text-4xl sm:mt-[-3%]"
+          data-aos="zoom-in"
+        >
           MOST POPULAR FILMS
         </h1>
         <div className="flex justify-between mt-4 sm:mt-[-20px] sm:mb-[20px]">
--- src/Components/Moviesimage/Movierow3.tsx
+++ src/Components/Moviesimage/Movierow3.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import VideoSlider from "../VideoSlider/VideoSlider";
 import VideoSlider2 from "../VideoSlider2/VideoSlider2";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Movierow3 = () => {
-
   return (
     <div>
       <div className="w-full h-100 mb-8 overflow-x-hidden sm:mt-[-45px]">
-        <h1 className="raleway text-2xl font-semibold text-center mt-1 pt-24 font-sans sm:text-3xl md:text-3xl lg:text-3xl xl:text-3xl 2xl:text-4xl sm:mt-[-3%]">
+        <h1
+          className="raleway text-2xl font-semibold text-center mt-1 pt-24 font-sans sm:text-3xl md:text-3xl lg:text-3xl mt-[-45px] xl:text-3xl 2xl:text-4xl sm:mt-[-3%]"
+          data-aos="zoom-in"
+        >
           COLLECTION
         </h1>
         <div className="flex justify-between mt-4 sm:mt-[-20px]">
